c0529153382b33004d2f1820bbe9da79772359ab
Richard Foil <103152246+RichardFoil@users.noreply.github.com>
2022-09-20 18:34:25 -0400

    code along exploring react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
 import './App.css'
 
 
@@ -34,14 +35,14 @@ const App = () => {
   }
 
   return (
-      <div>
-          <h1>{message}</h1>
-
-          <SearchBar handleSearch={handleSearch} />
-          
-          <Gallery data={data} />
-      </div>
-  )
+    <div className="App">
+        <SearchBar handleSearch={handleSearch} />
+        {message}
+        <DataContext.Provider value={data} >
+            <Gallery />
+        </DataContext.Provider>
+    </div>
+)
 }
 
 export default App
